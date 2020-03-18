c3fd0481bcb42aa4092fc6aee3be8e1ddc392be1
milanopatela <darkrai60@gmail.com>
2020-03-18 19:30:07 -0400

    Working on search functionality :#86 #87
    Trending in techincally done since the user can just filter their
    searches. Need to get dropwdown list working.

--- BaseProject#1/SOEN341InstagramReplica/SOEN341InstagramReplica/Controllers/HomeController.cs
+++ BaseProject#1/SOEN341InstagramReplica/SOEN341InstagramReplica/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using SOEN341InstagramReplica.Models;
 
 namespace SOEN341InstagramReplica.Controllers
@@ -212,5 +213,137 @@ namespace SOEN341InstagramReplica.Controllers
             }
             return View(posts.ToList());
         }
+
+        public ActionResult Trending3(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.DateSortParm = String.IsNullOrEmpty(sortOrder) ? "date_asc" : "";
+            ViewBag.TitleSortParm = sortOrder == "Title" ? "title_desc" : "Title";
+            ViewBag.LikesSortParm = sortOrder == "Likes" ? "likes_desc" : "Likes";
+            ViewBag.DislikesSortParm = sortOrder == "Dislikes" ? "dislikes_desc" : "Dislikes";
+            ViewBag.UserNameSortParm = sortOrder == "UserName" ? "userName_desc" : "UserName";
+
+            if(searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+            var posts = from x in db.UserPosts select x;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                posts = posts.Where(x => x.Title.Contains(searchString) || x.User.Username.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "date_asc":
+                    posts = posts.OrderBy(p => p.Date_Posted);
+                    break;
+                case "Title":
+                    posts = posts.OrderBy(p => p.Title);
+                    break;
+                case "title_desc":
+                    posts = posts.OrderByDescending(p => p.Title);
+                    break;
+                case "Likes":
+                    posts = posts.OrderBy(p => p.Likes);
+                    break;
+                case "likes_desc":
+                    posts = posts.OrderByDescending(p => p.Likes);
+                    break;
+                case "Dislikes":
+                    posts = posts.OrderBy(p => p.Dislikes);
+                    break;
+                case "dislikes_desc":
+                    posts = posts.OrderByDescending(p => p.Dislikes);
+                    break;
+                case "UserName":
+                    posts = posts.OrderBy(p => p.User.Username);
+                    break;
+                case "userName_desc":
+                    posts = posts.OrderByDescending(p => p.User.Username);
+                    break;
+                default:
+                    posts = posts.OrderByDescending(p => p.Date_Posted);
+                    break;
+            }
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(posts.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult Trending4(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            //List<string> list = new List<string>();
+            //list.Add("Post Title");
+            //list.Add("Username");
+            //list.Add("Both");
+            //ViewBag.PostAndOrUsername = new SelectList(list);
+            ViewBag.DateSortParm = String.IsNullOrEmpty(sortOrder) ? "date_asc" : "";
+            ViewBag.TitleSortParm = sortOrder == "Title" ? "title_desc" : "Title";
+            ViewBag.LikesSortParm = sortOrder == "Likes" ? "likes_desc" : "Likes";
+            ViewBag.DislikesSortParm = sortOrder == "Dislikes" ? "dislikes_desc" : "Dislikes";
+            ViewBag.UserNameSortParm = sortOrder == "UserName" ? "userName_desc" : "UserName";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+            var posts = from x in db.UserPosts select x;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                posts = posts.Where(x => x.Title.Contains(searchString) || x.User.Username.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "date_asc":
+                    posts = posts.OrderBy(p => p.Date_Posted);
+                    break;
+                case "Title":
+                    posts = posts.OrderBy(p => p.Title);
+                    break;
+                case "title_desc":
+                    posts = posts.OrderByDescending(p => p.Title);
+                    break;
+                case "Likes":
+                    posts = posts.OrderBy(p => p.Likes);
+                    break;
+                case "likes_desc":
+                    posts = posts.OrderByDescending(p => p.Likes);
+                    break;
+                case "Dislikes":
+                    posts = posts.OrderBy(p => p.Dislikes);
+                    break;
+                case "dislikes_desc":
+                    posts = posts.OrderByDescending(p => p.Dislikes);
+                    break;
+                case "UserName":
+                    posts = posts.OrderBy(p => p.User.Username);
+                    break;
+                case "userName_desc":
+                    posts = posts.OrderByDescending(p => p.User.Username);
+                    break;
+                default:
+                    posts = posts.OrderByDescending(p => p.Date_Posted);
+                    break;
+            }
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(posts.ToPagedList(pageNumber, pageSize));
+        }
+
     }
 }
